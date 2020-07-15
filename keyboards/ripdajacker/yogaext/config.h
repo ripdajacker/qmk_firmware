45e1f86e966ee667411021be000920139b3f5efc
Jesenko Mehmedbasic <jm@secata.com>
2020-07-15 14:17:26 +0200

    Working wooden keyboard layout and matrix scan using pulled-down rows with dual 74hc595 in series.

--- keyboards/ripdajacker/yogaext/config.h
+++ keyboards/ripdajacker/yogaext/config.h
@@ -3,29 +3,33 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    mehmedbasic
-#define PRODUCT         YogaExt
-#define DESCRIPTION     A keyboard with a Lenovo Yoga layout
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x6060
+#define DEVICE_VER 0x0001
+#define MANUFACTURER mehmedbasic
+#define PRODUCT YogaExt
+#define DESCRIPTION A keyboard with a Lenovo Yoga layout
 
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 17
 
-/* key matrix pins */
-#define MATRIX_ROW_PINS { D2, D3, D4, D5, D6, D7 }
-#define MATRIX_COL_PINS {}
+/* row pins */
+#define ROW_A  D1
+#define ROW_B  D0
+#define ROW_C  D4
+#define ROW_D  C6
+#define ROW_E  D7
+#define ROW_F  E6
 
 /* columns 0 - 16 */
 #define SHR_LATCH B2
 #define SHR_CLOCK B3
 #define SHR_DATA  B1
-#define SHR_COLS { 0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080,  0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000 }
+#define SHR_COLS { 0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080, 0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000 }
+#define COL_SEVENTEEN B6
 
 #define UNUSED_PINS
-
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -33,7 +37,7 @@
 /* number of backlight levels */
 
 #ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
+#    define BACKLIGHT_LEVELS 0
 #endif
 
 /* Set 0 if debouncing isn't needed */
@@ -46,9 +50,9 @@
 #define LOCKING_RESYNC_ENABLE
 
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 0
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
 #endif
--- keyboards/ripdajacker/yogaext/matrix.c
+++ keyboards/ripdajacker/yogaext/matrix.c
@@ -22,162 +22,165 @@
 #include "print.h"
 #include "debug.h"
 #include "util.h"
+#include "config.h"
 #include "matrix.h"
+#include "quantum.h"
 
 #ifndef DEBOUNCE
-#   define DEBOUNCE 5
+#    define DEBOUNCE 5
 #endif
+
+
+static const uint16_t col_values[16] = SHR_COLS;
+
 static uint8_t debouncing = DEBOUNCE;
 
 static matrix_row_t matrix[MATRIX_ROWS];
 static matrix_row_t matrix_debouncing[MATRIX_ROWS];
 
 static uint8_t read_rows(void);
-static void select_col(uint8_t col);
+static void    select_col(uint8_t col);
 
-__attribute__ ((weak))
-void matrix_init_kb(void) {
-    matrix_init_user();
-}
+static void    shift_pulse(void);
+static void    shift_out_single(uint8_t value);
+static void    shift_out(uint16_t value);
 
-__attribute__ ((weak))
-void matrix_scan_kb(void) {
-    matrix_scan_user();
-}
+__attribute__((weak)) void matrix_init_kb(void) { matrix_init_user(); }
 
-__attribute__ ((weak))
-void matrix_init_user(void) {
-}
+__attribute__((weak)) void matrix_scan_kb(void) { matrix_scan_user(); }
 
-__attribute__ ((weak))
-void matrix_scan_user(void) {
-}
+__attribute__((weak)) void matrix_init_user(void) {}
 
-inline uint8_t matrix_rows(void) {
-  return MATRIX_ROWS;
-}
+__attribute__((weak)) void matrix_scan_user(void) {}
 
-inline uint8_t matrix_cols(void) {
-  return MATRIX_COLS;
-}
+inline uint8_t matrix_rows(void) { return MATRIX_ROWS; }
 
-/* Column pin configuration
+inline uint8_t matrix_cols(void) { return MATRIX_COLS; }
+
+/**
+ * Row pins PD2, PD3... PD7 are input for rows A-F.
+ * The rows are pulled low with a pull-down resistor.
  *
- * col: 0    1    2    3    4    5    6    7
- * pin: PC7  PD5  PD3  PD1  PC2  PD6  PD4  PD2
- *
- * Rrr pin configuration
- *
- * These rrrs uses one 74HC154 4 to 16 bit demultiplexer (low
- * active), together with 2 rrrs driven directly from the micro
- * controller, to control the 18 rrrs. The rrrs are driven from
- * pins B6,5,4,3,2,1,0.
+ * The columns are scanned using two 74hc595 in series on pins defined in config.h.
  */
 void matrix_init(void) {
-  DDRC  &= ~0b10000100;  // Row input pins
-  DDRD  &= ~0b01111110;
-  PORTC |=  0b10000100;
-  PORTD |=  0b01111110;
+    setPinInput(D2);
+    setPinInput(D3);
+    setPinInput(D4);
+    setPinInput(D5);
+    setPinInput(D6);
+    setPinInput(D7);
 
-  DDRB  |=  0b01111111;  // Column output pins
+    setPinOutput(SHR_DATA);
+    setPinOutput(SHR_LATCH);
+    setPinOutput(SHR_CLOCK);
+    setPinOutput(COL_SEVENTEEN);
 
-  for (uint8_t i=0; i < MATRIX_ROWS; i++)  {
-    matrix[i] = 0;
-    matrix_debouncing[i] = 0;
-  }
-  matrix_init_quantum();
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        matrix[i]            = 0;
+        matrix_debouncing[i] = 0;
+    }
+    matrix_init_quantum();
 }
 
 uint8_t matrix_scan(void) {
-  for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-    select_col(col);
-    _delay_us(3);
-    uint8_t rows = read_rows();
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-      bool prev_bit = matrix_debouncing[row] & ((matrix_row_t)1<<col);
-      bool curr_bit = rows & (1<<row);
-      if (prev_bit != curr_bit) {
-        matrix_debouncing[row] ^= ((matrix_row_t)1<<col);
-        debouncing = DEBOUNCE;
-      }
+    for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+        select_col(col);
+        _delay_us(3);
+        uint8_t rows = read_rows();
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+            bool prev_bit = matrix_debouncing[row] & ((matrix_row_t)1 << col);
+            bool curr_bit = rows & (1 << row);
+            if (prev_bit != curr_bit) {
+                matrix_debouncing[row] ^= ((matrix_row_t)1 << col);
+                debouncing = DEBOUNCE;
+            }
+        }
     }
-  }
 
-  if (debouncing) {
-    if (--debouncing) {
-      _delay_ms(1);
+    if (debouncing) {
+        if (--debouncing) {
+            _delay_ms(1);
+        } else {
+            for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+                matrix[i] = matrix_debouncing[i];
+            }
+        }
     }
-    else {
-      for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-        matrix[i] = matrix_debouncing[i];
-      }
-    }
-  }
-  matrix_scan_quantum();
-  return 1;
+    matrix_scan_quantum();
+    return 1;
 }
 
 bool matrix_is_modified(void) {
-  if (debouncing)
-    return false;
-  else
-    return true;
+    if (debouncing)
+        return false;
+    else
+        return true;
 }
 
-inline bool matrix_is_on(uint8_t row, uint8_t col) {
-  return (matrix[row] & ((matrix_row_t)1<<col));
-}
+inline bool matrix_is_on(uint8_t row, uint8_t col) { return (matrix[row] & ((matrix_row_t)1 << col)); }
 
-inline matrix_row_t matrix_get_row(uint8_t row) {
-  return matrix[row];
-}
+inline matrix_row_t matrix_get_row(uint8_t row) { return matrix[row]; }
 
 void matrix_print(void) {
-  print("\nr/c 0123456789ABCDEF\n");
-  for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-    xprintf("%02X: %032lb\n", row, bitrev32(matrix_get_row(row)));
-  }
+    print("\nr/c 0123456789ABCDEF\n");
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        xprintf("%02X: %032lb\n", row, bitrev32(matrix_get_row(row)));
+    }
 }
 
 uint8_t matrix_key_count(void) {
-  uint8_t count = 0;
-  for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-    count += bitpop32(matrix[i]);
-  }
-  return count;
+    uint8_t count = 0;
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        count += bitpop32(matrix[i]);
+    }
+    return count;
 }
 
 static uint8_t read_rows(void) {
-  return
-    (PINC&(1<<7) ? 0 : (1<<0)) |
-    (PIND&(1<<5) ? 0 : (1<<1)) |
-    (PIND&(1<<3) ? 0 : (1<<2)) |
-    (PIND&(1<<1) ? 0 : (1<<3)) |
-    (PINC&(1<<2) ? 0 : (1<<4)) |
-    (PIND&(1<<2) ? 0 : (1<<5)) |
-    (PIND&(1<<4) ? 0 : (1<<6)) |
-    (PIND&(1<<6) ? 0 : (1<<7));
+     return (readPin(ROW_F) << 5)
+     | (readPin(ROW_E) << 4)
+     | (readPin(ROW_D) << 3)
+     | (readPin(ROW_C) << 2)
+     | (readPin(ROW_B) << 1)
+     | (readPin(ROW_A) );
+}
+
+static inline void shift_pulse(void) {
+    writePinHigh(SHR_CLOCK);
+    writePinLow(SHR_CLOCK);
+}
+
+static void shift_out_single(uint8_t value) {
+    for (uint8_t i = 0; i < 8; i++) {
+        if (value & 0b10000000) {
+            writePinHigh(SHR_DATA);
+        } else {
+            writePinLow(SHR_DATA);
+        }
+
+        shift_pulse();
+        value = value << 1;
+    }
+}
+
+static void shift_out(uint16_t value) {
+    writePinLow(SHR_LATCH);
+    uint8_t first_byte  = (value >> 8) & 0xFF;
+    uint8_t second_byte = (uint8_t)(value & 0xFF);
+
+    shift_out_single(first_byte);
+    shift_out_single(second_byte);
+    writePinHigh(SHR_LATCH);
 }
 
 static void select_col(uint8_t col) {
-  switch (col) {
-  case  0: PORTB = (PORTB & ~0b01111111) | 0b01100100; break;
-  case  1: PORTB = (PORTB & ~0b01111111) | 0b01101100; break;
-  case  2: PORTB = (PORTB & ~0b01111111) | 0b01100010; break;
-  case  3: PORTB = (PORTB & ~0b01111111) | 0b01111010; break;
-  case  4: PORTB = (PORTB & ~0b01111111) | 0b01100110; break;
-  case  5: PORTB = (PORTB & ~0b01111111) | 0b01110110; break;
-  case  6: PORTB = (PORTB & ~0b01111111) | 0b01101110; break;
-  case  7: PORTB = (PORTB & ~0b01111111) | 0b01111110; break;
-  case  8: PORTB = (PORTB & ~0b01111111) | 0b01000001; break;
-  case  9: PORTB = (PORTB & ~0b01111111) | 0b00100001; break;
-  case 10: PORTB = (PORTB & ~0b01111111) | 0b01101010; break;
-  case 11: PORTB = (PORTB & ~0b01111111) | 0b01110010; break;
-  case 12: PORTB = (PORTB & ~0b01111111) | 0b01111100; break;
-  case 13: PORTB = (PORTB & ~0b01111111) | 0b01110100; break;
-  case 14: PORTB = (PORTB & ~0b01111111) | 0b01111000; break;
-  case 15: PORTB = (PORTB & ~0b01111111) | 0b01110000; break;
-  case 16: PORTB = (PORTB & ~0b01111111) | 0b01100000; break;
-  case 17: PORTB = (PORTB & ~0b01111111) | 0b01101000; break;
-  }
+    // SHIFT out columns 0 to 15
+    if (col < 16) {
+        shift_out(col_values[col]);
+        writePinLow(COL_SEVENTEEN);
+    } else {
+        shift_out(0);
+        writePinHigh(COL_SEVENTEEN);
+    }
 }
